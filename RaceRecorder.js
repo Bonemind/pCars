5f1cd26629da174277a7032e327e74449a5d3b9c
Bonemind <Subhime@gmail.com>
2015-05-12 19:59:01 +0200

    Implemented a track data recorder

--- RaceRecorder.js
+++ RaceRecorder.js
@@ -3,6 +3,7 @@ var lowdb = require("lowdb");
 var CrestReader = require("./lib/CrestReader.js");
 var ReplayReader = require("./lib/ReplayReader.js");
 var RaceLogger = require("./lib/RaceLogger.js");
+var TrackRecorder = require("./lib/TrackRecorder.js");
 
 /* var crestReader = new CrestReader();
 crestReader.on("data-update", processJson);
@@ -10,11 +11,14 @@ crestReader.startAutoPoll();*/
 
 var crestReader = new CrestReader();
 var raceLogger = new RaceLogger();
+var trackRecorder = new TrackRecorder();
 raceLogger.setDataProvider(crestReader);
+trackRecorder.setDataProvider(crestReader);
 crestReader.on("replay-done", function() {
 	console.log("replay-done");
 });
 raceLogger.start();
+trackRecorder.start();
 crestReader.startAutoPoll();
 
 
